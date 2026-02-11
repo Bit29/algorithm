5593891c1f871fcc153749f58adcf8c5faaaf692
Bit29 <51738460+Bit29@users.noreply.github.com>
2026-02-11 16:01:46 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+def solution():
+    n=int(input())
+    A=list(map(int,input().split(" ")))
+    B=list(map(int,input().split(" ")))
+    A.sort(reverse=True)
+    B.sort()
+    zip(A,B)
+    answer=sum([a*b for a,b in zip(A,B)])
+    print(answer)
+    return
+if __name__ == "__main__":
+    solution()
